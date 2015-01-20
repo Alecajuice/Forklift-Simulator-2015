e3ea2abfb9b38ed7863ef44747f09104320cd428
James Ngo <ducngocngo2@gmail.com>
2015-01-20 12:53:10 -0800

    commiting the correct code (with mainloop)

--- Forklift Simulator 2015/src/main/ForkliftSim2015.java
+++ Forklift Simulator 2015/src/main/ForkliftSim2015.java
@@ -1,6 +1,7 @@
 package main;
 
-
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.openal.AL10.*;
 
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.openal.AL10.*;
@@ -15,14 +16,15 @@ import org.lwjgl.opengl.SharedDrawable;
 
 public class ForkliftSim2015 {
 	
-	public final int SCREEN_WIDTH = 1000;
-	public final int SCREEN_HEIGHT = 1000;
+	public final int SCREEN_WIDTH = 700;
+	public final int SCREEN_HEIGHT = 700;
+	public final int UPDATE_SPEED = 60;
 
 	public static void main(String[] args) {
 		new ForkliftSim2015();
 	}
 	
-	public ForkliftSim2015() {
+	private ForkliftSim2015() {
 			
 			try {
 				Display.setDisplayMode(new DisplayMode(SCREEN_WIDTH, SCREEN_HEIGHT));
@@ -30,6 +32,7 @@ public class ForkliftSim2015 {
 			} catch (LWJGLException e) {
 				System.err.print("Err 1:");
 				e.printStackTrace();
+	            System.exit(0);
 			}
 			
 			glMatrixMode(GL_PROJECTION);
@@ -37,7 +40,34 @@ public class ForkliftSim2015 {
 			glOrtho(0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 1, -1);
 			glMatrixMode(GL_MODELVIEW);
 			glEnable(GL_TEXTURE_2D);
+		
+			mainLoop();
 			
+			Display.destroy();
+			System.exit(0);
+	}
+	
+	private void mainLoop() {
+		
+		while(!Display.isCloseRequested()) {
+			
+			update();
+			render();
+		}
+	}
+
+	private void update() {
+
+		System.out.println(getMouse);
+
 		
 	}
+
+	private void render() {
+
+		Display.update();
+		Display.sync(UPDATE_SPEED);
+		
+	}
+	
 }

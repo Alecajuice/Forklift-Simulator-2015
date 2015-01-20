efc15f6776e0f95ff0843b2733cfb7c6e2787ce4
James Ngo <ducngocngo2@gmail.com>
2015-01-20 12:29:30 -0800

    Changed default settings of the window

--- Forklift Simulator 2015/src/main/ForkliftSim2015.java
+++ Forklift Simulator 2015/src/main/ForkliftSim2015.java
@@ -2,9 +2,16 @@ package main;
 
 
 
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.openal.AL10.*;
+
 import org.lwjgl.LWJGLException;
+import org.lwjgl.openal.AL;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.Drawable;
+import org.lwjgl.opengl.SharedDrawable;
 
 public class ForkliftSim2015 {
 	
@@ -24,6 +31,13 @@ public class ForkliftSim2015 {
 				System.err.print("Err 1:");
 				e.printStackTrace();
 			}
+			
+			glMatrixMode(GL_PROJECTION);
+			glLoadIdentity();
+			glOrtho(0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 1, -1);
+			glMatrixMode(GL_MODELVIEW);
+			glEnable(GL_TEXTURE_2D);
+			
 		
 	}
 }

af4e931c343ab2a1ea4087b5f648df2c62a0eda1
James Ngo <ducngocngo2@gmail.com>
2015-02-26 14:00:24 -0800

    worked on matrix multiplication

--- Forklift Simulator 2015/src/graphics/Transformations.java
+++ Forklift Simulator 2015/src/graphics/Transformations.java
@@ -2,45 +2,19 @@ package graphics;
 
 public class Transformations {
 
-	public static Vector xYRot(Vector vec, double radians) {
-		double[] matrix = {
-				Math.cos(radians), -Math.sin(radians), 0,
-				Math.sin(radians), Math.cos(radians), 0,
-				0, 0, 1
-		};
+	public static void multMatrices(int[]x, int[] y, int xrows, int yrows) {
 		
-		double newX = vec.getX()*matrix[0] + vec.getY()*matrix[1] + vec.getZ()*matrix[2];
-		double newY = vec.getX()*matrix[3] + vec.getY()*matrix[4] + vec.getZ()*matrix[5];
-		double newZ = vec.getX()*matrix[6] + vec.getY()*matrix[7] + vec.getZ()*matrix[8];
 		
-		return new Vector((int)newX, (int)newY, (int)newZ);
 	}
 	
-	public static Vector xZRot(Vector vec, double radians) {
-		double[] matrix = {
-				Math.cos(radians), 0, Math.sin(radians),
-				0, 1, 0,
-				-Math.sin(radians), 0, Math.cos(radians)
-		};
-		
-		double newX = vec.getX()*matrix[0] + vec.getY()*matrix[1] + vec.getZ()*matrix[2];
-		double newY = vec.getX()*matrix[3] + vec.getY()*matrix[4] + vec.getZ()*matrix[5];
-		double newZ = vec.getX()*matrix[6] + vec.getY()*matrix[7] + vec.getZ()*matrix[8];
-		
-		return new Vector((int)newX, (int)newY, (int)newZ);
-	}
-	
-	public static Vector yZRot(Vector vec, double radians) {
-		double[] matrix = {
-				Math.cos(radians), -Math.sin(radians), 0,
-				Math.sin(radians), Math.cos(radians), 0,
-				0, 0, 1
-		};
-		
-		double newX = vec.getX()*matrix[0] + vec.getY()*matrix[1] + vec.getZ()*matrix[2];
-		double newY = vec.getX()*matrix[3] + vec.getY()*matrix[4] + vec.getZ()*matrix[5];
-		double newZ = vec.getX()*matrix[6] + vec.getY()*matrix[7] + vec.getZ()*matrix[8];
-		
-		return new Vector((int)newX, (int)newY, (int)newZ);
+	public static void rowByCol(int[]x, int[] y, int xrows, int yrows, int xrow, int ycol) {
+		int retval = 0;
+		for(int i = 0; i < x.length; i++) {
+			if(i >= (xrow-1)*(x.length/xrows) && i < xrow*(x.length/xrows)) {
+				for(int j = 0; i < y.length; i++) {
+					if()
+				}
+			}
+		}
 	}
 }
